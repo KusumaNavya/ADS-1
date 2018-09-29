89d9018b80848d6519825e7a89c93110ca96fc7f
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-29 12:40:14 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- Assignments/M6/Assignment-1/LinkedList.java
+++ Assignments/M6/Assignment-1/LinkedList.java
@@ -7,19 +7,19 @@ class LinkedList {
 		this.size = 0;
 	}
 
-	public void pushRight(int name) {
-		if (start == null) {
-			start = new Node(name);
-		} else {
-			Node temp = start;
-			Node a = new Node(name);
-			while (temp.getNextNode() != null) {
-				temp = temp.getNextNode();	
-			}
-			temp.setNextNode(a);
-		}
-		size++;
-	}
+	// public void pushRight(int name) {
+	// 	if (start == null) {
+	// 		start = new Node(name);
+	// 	} else {
+	// 		Node temp = start;
+	// 		Node a = new Node(name);
+	// 		while (temp.getNextNode() != null) {
+	// 			temp = temp.getNextNode();	
+	// 		}
+	// 		temp.setNextNode(a);
+	// 	}
+	// 	size++;
+	// }
 	public void pushLeft(int name) {
 		Node newnode = new Node(name);
 		newnode.setNextNode(start);
@@ -39,41 +39,38 @@ class LinkedList {
 			System.out.println("No nodes to remove");
 		}
 	}
-	public void popRight() {
-		if (start == null) {
-			System.out.println("No nodes to remove");
-		} else if (size < 2) {
-			start = null;
-			size--;
-		} else {
-			Node temp = start;
-			while (temp.getNextNode().getNextNode() != null) {
-				temp = temp.getNextNode();
-			}
-			temp.setNextNode(null);
-			size--;
-		}
-	}
-	public void remove(int name) {
-		Node temp = start;
+	// public void popRight() {
+	// 	if (start == null) {
+	// 		System.out.println("No nodes to remove");
+	// 	} else if (size < 2) {
+	// 		start = null;
+	// 		size--;
+	// 	} else {
+	// 		Node temp = start;
+	// 		while (temp.getNextNode().getNextNode() != null) {
+	// 			temp = temp.getNextNode();
+	// 		}
+	// 		temp.setNextNode(null);
+	// 		size--;
+	// 	}
+	// }
+	// public void remove(int name) {
+	// 	Node temp = start;
 		
-			while (temp.getNextNode() != null) {
-				temp = temp.getNextNode();
-				if(name == temp.getName()) {
-					temp = temp.getNextNode().getNextNode();
-				}	
-			}
-	}
+	// 		while (temp.getNextNode() != null) {
+	// 			temp = temp.getNextNode();
+	// 			if(name == temp.getName()) {
+	// 				temp = temp.getNextNode().getNextNode();
+	// 			}	
+	// 		}
+	// }
 	public String toString() {
 		String s = "";
 		Node temp = start;
-		if (temp == null) {
-			return "Empty linked list";
-		}
 		while (temp.getNextNode() != null) {
-			s += temp.getName() + ", ";
+			s += temp.getName();
 			temp = temp.getNextNode();
-		} s += temp.getName();
+		}
 		return s;
 	}
 }
