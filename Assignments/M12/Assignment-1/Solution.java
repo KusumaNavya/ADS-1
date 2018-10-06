cb9cd0583a2f267ce98b103196e006477e001472
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-10-06 11:14:37 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/M12/Assignment-1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        int i = 6;
+        while (sc.hasNext()) {
+            sb.append(sc.nextLine() + "#");
+        }
+        String[] lines = sb.toString().split("#");
+        Student[] students = new Student[lines.length];
+        for (String line : lines) {
+            String[] tokens = line.split(",");
+            students[i++] = new Student(tokens[0],
+                Integer.parseInt(tokens[1]),
+                Integer.parseInt(tokens[2]),
+                Integer.parseInt(tokens[2 + 1]),
+                Integer.parseInt(tokens[2 + 2]),
+                Integer.parseInt(tokens[5]),
+                tokens[6]);
+        }
+        Insertion.sort(students);
+        String result = Arrays.toString(students);
+        result = result.replace("[", "").replace("]", "").replace(", ", ",");
+        System.out.println(result);
+	}
+}
