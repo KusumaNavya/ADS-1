2cc0286043b794b9d7913b704e96b6ba1539d18c
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-10-27 11:49:51 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/M24/1/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public final class Solution {
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		final int three = 3;
+    	Scanner scan = new Scanner(System.in);
+	    int lines = Integer.parseInt(scan.nextLine());
+	    LinearProbingHashST<Integer, Student> part =
+	      new LinearProbingHashST<>(three * lines);
+	    for (int i = 0; i < lines; i++) {
+	      	String[] tokens = scan.nextLine().split(",");
+	      	Student list = new Student(Integer.parseInt(tokens[0]),
+	                                 tokens[1],
+	                                 Double.parseDouble(tokens[2]));
+	      	part.put(Integer.parseInt(tokens[0]), list);
+	    }
+	    int querys = Integer.parseInt(scan.nextLine());
+	    for (int j = 0; j < querys; j++) {
+	        String[] tokens = scan.nextLine().split(" ");
+	      	switch (tokens[2]) {
+	      	case "1":
+	          	System.out.println(part.get(
+	                               Integer.parseInt(tokens[1])).getName());
+	        	break;
+	      	case "2":
+	          		System.out.println(part.get(
+	                               Integer.parseInt(tokens[1])).getMarks());
+	        	break;
+	      	default:
+	        break;
+			}
+		}
+	}
+}
---
+++ Assignments/M24/1/Student.java
@@ -0,0 +1,60 @@
+/**
+ * Class for student.
+ */
+class Student {
+	/**
+	 * {rollnumber of the student}
+	 */
+	private int rollnumber;
+	/**
+	 * {name of the student}
+	 */
+	private String name;
+	/**
+	 * {marks obtained by the student}
+	 */
+	private double marks;
+	/**
+	 *empty constructor.
+	 */
+	Student() {
+		//empty constructor
+	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      namee   The namee
+	 * @param      rollno  The rollno
+	 * @param      markss  The markss
+	 */
+	public Student(final int rollno, final String namee, final double markss) {
+		this.rollnumber = rollno;
+		this.name = namee;
+		this.marks = markss;
+	}
+	/**
+	 * {get method for name}
+	 *
+	 * @return     {returns name of the student}
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * {get method for roll number}
+	 *
+	 * @return     {returns rollnumber of the student}
+	 */
+	public int getRollno() {
+		return rollnumber;
+	}
+
+	/**
+	 * {get method for marks}
+	 *
+	 * @return     {returns marks obtained by the students}
+	 */
+	public double getMarks() {
+		return marks;
+	}
+}
