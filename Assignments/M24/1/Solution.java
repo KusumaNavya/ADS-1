bd96890f654e91433e7bec628ce47e01b7103417
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-10-27 12:01:34 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M24/1/Solution.java
+++ Assignments/M24/1/Solution.java
@@ -32,12 +32,20 @@ public final class Solution {
             String[] tokens = scan.nextLine().split(" ");
             switch (tokens[2]) {
             case "1":
-                System.out.println(part.get(
+                try {
+                    System.out.println(part.get(
                         Integer.parseInt(tokens[1])).getName());
+                } catch(Exception e) {
+                    System.out.println("Student doesn't exists...");
+                }
                 break;
             case "2":
-                    System.out.println(part.get(
+                    //try {
+                        System.out.println(part.get(
                         Integer.parseInt(tokens[1])).getMarks());
+                    //} catch(Exception e) {
+                        //System.out.println("Student doesn't exists...");
+                    //}
                 break;
             default:
             break;
