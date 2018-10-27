fd30e049c77cbd767afbf850fcfd7dd0ed7adf67
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-10-27 12:44:22 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M24/2/Solution.java
+++ Assignments/M24/2/Solution.java
@@ -15,17 +15,40 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(String[] args) {
-        final int three = 3;
         Scanner scan = new Scanner(System.in);
         int lines = Integer.parseInt(scan.nextLine());
         LinearProbingHashST<Integer, Student> part =
-                new LinearProbingHashST<>(three * lines);
+                new LinearProbingHashST<>(lines);
         for (int i = 0; i < lines; i++) {
             String[] tokens = scan.nextLine().split(",");
             Student list = new Student(Integer.parseInt(tokens[0]),
                                      tokens[1],
                                      Double.parseDouble(tokens[2]));
             part.put(Integer.parseInt(tokens[0]), list);
+        }
+        int querys = Integer.parseInt(scan.nextLine());
+        for (int j = 0; j < querys; j++) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[2]) {
+            case "1":
+                try {
+                    System.out.println(part.get(
+                        Integer.parseInt(tokens[1])).getName());
+                } catch(Exception e) {
+                    System.out.println("Student doesn't exists...");
+                }
+                break;
+            case "2":
+                try {
+                    System.out.println(part.get(
+                        Integer.parseInt(tokens[1])).getMarks());
+                } catch(Exception e) {
+                    System.out.println("Student doesn't exists...");
+                }
+                break;
+            default:
+            break;
+            }
         }
     }
 }
